9b8ed14c1d3c2c2f899e90d711bc8bbfc134e6fa
Daniil-Solovev <frontend.servise@gmail.com>
2017-11-13 16:33:31 +0300

    Выполнена итерация по массивам v.2

--- index.php
+++ index.php
@@ -72,24 +72,66 @@ $lot_time_remaining = sprintf('%02d', $hours) . ':' . sprintf('%02d', $minutes);
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            <?=$lot_time_remaining;?>
-                        </div>
-                    </div>
-                </div>
-            </li>
+          <?php
+            $lots__list = [
+              [
+                'lot_name' => '2014 Rossignol District Snowboard',
+                'lot_category' => 'Доски и лыжи',
+                'lot_price' => '10999',
+                'lot_url' => 'img/lot-1.jpg'
+              ],
+              [
+                'lot_name' => 'DC Ply Mens 2016/2017 Snowboard',
+                'lot_category' => 'Доски и лыжи',
+                'lot_price' => '159999',
+                'lot_url' => 'img/lot-2.jpg'
+              ],
+              [
+                'lot_name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+                'lot_category' => 'Крепления',
+                'lot_price' => '8000',
+                'lot_url' => 'img/lot-3.jpg'
+              ],
+              [
+                'lot_name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+                'lot_category' => 'Ботинки',
+                'lot_price' => '10999',
+                'lot_url' => 'img/lot-4.jpg'
+              ],
+              [
+                'lot_name' => 'Куртка для сноуборда DC Mutiny Charocal',
+                'lot_category' => 'Одежда',
+                'lot_price' => '7500',
+                'lot_url' => 'img/lot-5.jpg'
+              ],
+              [
+                'lot_name' => 'Маска Oakley Canopy',
+                'lot_category' => 'Разное',
+                'lot_price' => '5400',
+                'lot_url' => 'img/lot-6.jpg'
+              ]
+            ]
+           ?>
+           <?php foreach ($lots__list  as $key => $value): ?>
+              <li class="lots__item lot">
+                  <div class="lot__image">
+                      <img src="<?=$value['lot_url']?>" width="350" height="260" alt="Сноуборд">
+                  </div>
+                  <div class="lot__info">
+                      <span class="lot__category"><?=$value['lot_category']?></span>
+                      <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['lot_name']?></a></h3>
+                      <div class="lot__state">
+                          <div class="lot__rate">
+                              <span class="lot__amount">Стартовая цена</span>
+                              <span class="lot__cost"><?=$value['lot_price']?><b class="rub">р</b></span>
+                          </div>
+                          <div class="lot__timer timer">
+                              <?=$lot_time_remaining;?>
+                          </div>
+                      </div>
+                  </div>
+              </li>
+            <?php endforeach ?>
         </ul>
     </section>
 </main>
