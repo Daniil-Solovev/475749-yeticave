a651cf44cdb0883f3bf8d7f992b97bb13d3387cc
Daniil-Solovev <frontend.servise@gmail.com>
2017-11-13 14:46:09 +0300

    Выполнена итерация по массивам v.1

--- index.php
+++ index.php
@@ -55,24 +55,16 @@ $lot_time_remaining = sprintf('%02d', $hours) . ':' . sprintf('%02d', $minutes);
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="promo__item promo__item--attachment">
-                <a class="promo__link" href="all-lots.html">Крепления</a>
-            </li>
-            <li class="promo__item promo__item--boots">
-                <a class="promo__link" href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="promo__item promo__item--clothing">
-                <a class="promo__link" href="all-lots.html">Одежда</a>
-            </li>
-            <li class="promo__item promo__item--tools">
-                <a class="promo__link" href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="promo__item promo__item--other">
-                <a class="promo__link" href="all-lots.html">Разное</a>
-            </li>
+            <?php
+              $category = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+              $cat__count = count($category);
+
+              foreach ($category as $value) {
+                print('<li class="promo__item promo__item--tools">');
+                print("<a class='promo__link' href='all-lots.html'> $value </a>");
+                print('</li>');
+              }
+             ?>
         </ul>
     </section>
     <section class="lots">
