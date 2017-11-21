ad51f1de15c413499fe89ade39526c41f42bf192
Daniil-Solovev <frontend.servise@gmail.com>
2017-11-21 15:12:24 +0300

    Выполнена шаблонизация проекта v.2 (доработка)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   require_once('functions.php');
   require_once('app/init.php');
 
-  $page__content = renderTemplate('templates/main.php', ['categories' => $categories, 'lots__list' => $lots__list, 'lot_time_remaining' => $lot_time_remaining]);
+  $page__content = renderTemplate('templates/main.php', ['categories' => $categories, 'lots__list' => $lots__list]);
   $page__layout = renderTemplate('templates/layout.php', ['page__content' => $page__content, 'title' => 'Yeticave']);
   print($page__layout);
 
