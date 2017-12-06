d8c1c60fc0dba0c16e3a5d0700b19e8cb612c5af
Daniil-Solovev <frontend.servise@gmail.com>
2017-12-06 14:14:19 +0300

    Выполнено создание схемы БД личного проекта v.2

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `category` (
-`id` int(5000) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id` int(50) UNSIGNED NOT NULL AUTO_INCREMENT,
 `cat_name` char(100) NOT NULL,
 PRIMARY KEY (`id`) ,
-UNIQUE INDEX `cat_name` (`cat_name` ASC)
+UNIQUE INDEX `idx_cat_name` (`cat_name` ASC)
 );
 
 CREATE TABLE `lot` (
@@ -14,12 +14,15 @@ CREATE TABLE `lot` (
 `lot_rate` tinyint(20) NOT NULL,
 `lot_step` tinyint(20) NOT NULL,
 `lot_date` date NOT NULL,
-`author` char(50) NOT NULL,
-`winner` char(50) NOT NULL,
-`category_id` char(10) NOT NULL,
+`author` int(5000) NOT NULL,
+`winner` int(5000) NOT NULL,
+`category_id` int(50) NOT NULL,
 PRIMARY KEY (`id`) ,
-UNIQUE INDEX `category_id` (`category_id` ASC),
-INDEX `lot_name` (`lot_name` ASC)
+INDEX `idx_category_id` (`category_id` ASC),
+INDEX `idx_lot_id` (`id` ASC),
+FOREIGN KEY (`winner`) REFERENCES `user` (`id`),
+FOREIGN KEY (`author`) REFERENCES `user` (`id`),
+FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
 );
 
 CREATE TABLE `bet` (
@@ -29,11 +32,12 @@ CREATE TABLE `bet` (
 `user_id` int(5000) NOT NULL,
 `lot_id` bit(5000) NOT NULL,
 PRIMARY KEY (`id`) ,
-UNIQUE INDEX `user_id` (`user_id` ASC),
-UNIQUE INDEX `lot_id` (`lot_id` ASC)
+UNIQUE INDEX `idx_user_lot` (`user_id` ASC, `lot_id` ASC),
+FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+FOREIGN KEY (`lot_id`) REFERENCES `lot` (`id`)
 );
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
 `id` int(5000) UNSIGNED NOT NULL AUTO_INCREMENT,
 `register` date NOT NULL,
 `email` char(50) NOT NULL,
@@ -41,19 +45,8 @@ CREATE TABLE `user` (
 `password` char(50) NOT NULL,
 `avatar` char(50) NULL,
 `contacts` text NULL,
-`lot_id` int(5000) NOT NULL,
-`bets` int(5000) NOT NULL,
 PRIMARY KEY (`id`) ,
-UNIQUE INDEX `email` (`email` ASC),
-UNIQUE INDEX `lot_id` (`lot_id` ASC)
+UNIQUE INDEX `idx_email` (`email` ASC)
 );
 
-
-ALTER TABLE `lot` ADD CONSTRAINT `fk_lot_user_1` FOREIGN KEY (`winner`) REFERENCES `user` (`id`);
-ALTER TABLE `lot` ADD CONSTRAINT `fk_lot_user_2` FOREIGN KEY (`author`) REFERENCES `user` (`id`);
-ALTER TABLE `bet` ADD CONSTRAINT `fk_bet_user_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-ALTER TABLE `user` ADD CONSTRAINT `fk_user_lot_1` FOREIGN KEY (`lot_id`) REFERENCES `lot` (`id`);
-ALTER TABLE `user` ADD CONSTRAINT `fk_user_bet_1` FOREIGN KEY (`bets`) REFERENCES `bet` (`id`);
-ALTER TABLE `lot` ADD CONSTRAINT `fk_lot_category_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
-ALTER TABLE `bet` ADD CONSTRAINT `fk_bet_lot_1` FOREIGN KEY (`lot_id`) REFERENCES `lot` (`id`);
 
