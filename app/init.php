445e17213d537ede0e5416b7249f242181ea9c34
Daniil-Solovev <frontend.servise@gmail.com>
2017-12-07 22:14:51 +0300

    Выполнено одключение к MySQL

--- app/init.php
+++ app/init.php
@@ -3,14 +3,17 @@
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/app/data.php' );
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/app/functions.php' );
 
-$is_auth = (bool) rand(0, 1);
-
-$user_name = 'Константин';
-$user_avatar = 'img/user.jpg';
-
 // устанавливаем часовой пояс в Московское время
 date_default_timezone_set('Europe/Moscow');
 
 session_start();
 
 $authorizedUser = getAuthorizedUser();
+
+
+if ($link = mysqli_connect("localhost", "root", "", "Yeticave")) {
+    mysqli_set_charset($link, "utf8");
+} else {
+    $error = mysqli_connect_error();
+}
+
--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/app/init.php');
 
-$page__content = renderTemplate('templates/main.php', ['categories' => $categories, 'lots__list' => $lots__list]);
+if (!$link) {
+    $error = mysqli_connect_error();
+    $page__content = renderTemplate('templates/error.php', ['error' => $error]);
+
+} else {
+    $page__content = renderTemplate('templates/main.php', ['categories' => $categories, 'lots__list' => $lots__list]);
+}
+
+
 $page__layout = renderTemplate('templates/layout.php', ['page__content' => $page__content, 'title' => 'Yeticave', 'categories' => $categories, 'authorizedUser' => $authorizedUser]);
 print($page__layout);
